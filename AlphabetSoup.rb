98e597a6300f2e90429151d030c65a5035e041da
ParkSeongha <firstclass@kaist.ac.kr>
2015-08-04 18:42:48 +0900

    Create AlphabetSoup.rb

---
+++ AlphabetSoup.rb
@@ -0,0 +1,11 @@
+def AlphabetSoup(str)
+  
+  return str.chars.sort.join
+
+end
+         
+
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets) 
